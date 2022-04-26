f4f79bda53c89d0efbc133f03d9d60d6dfe176e1
maiconmartins <maicon292010@gmail.com>
2022-04-26 20:47:17 +0000

    connection to mongodb

---
+++ models/bucketList.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+var Bucketschema = new mongoose.Schema({
+
+    name: {
+        type: String
+    },
+    nationality: {
+        type: String
+    },
+    age: {
+        type: String
+
+    },
+    dream: {
+        type: String
+    }
+
+
+});
+
+mongoose.model('bucket' ,Bucketschema);
